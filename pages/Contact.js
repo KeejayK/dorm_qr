b095b1eb1113c9811f400ecb1987e46a4388138b
Keejay <KeejayK@gmail.com>
2022-08-26 17:56:53 -0700

    im going insane

---
+++ pages/Contact.js
@@ -0,0 +1,59 @@
+
+import styles from '../styles/Contact.module.css'
+import Image from 'next/image'
+ 
+const Contact = () => {
+    return (  
+        <div className={styles.contact}>
+            <div className={styles.jaeContact}>
+            <div className={styles.logo}>
+                <Image src='/pfp.png' width={100} height={100}/>
+                </div>
+                <div>
+                    <h3 className = {styles.info}>Jaechan Lee</h3>
+                    <div className={styles.links}>
+                        <a href='https://www.instagram.com/invites/contact/?i=1rasrnl6oit7b&utm_content=38yfz1h'>
+                            <Image src='/insta.png' width={40} height={40}/>
+                        </a>
+                        <a href='https://www.snapchat.com/add/keejaykim?share_id=L1zGgiZmtcQ&locale=en-US'>
+                            <Image src='/snap.png' width={40} height={40}/>
+                        </a>
+                        <a href='https://www.snapchat.com/add/keejaykim?share_id=L1zGgiZmtcQ&locale=en-US'>
+                            <Image src='/spotify-icon-0.png' width={40} height={40}/>
+                        </a>
+                    </div>
+                 
+                </div>
+               
+            </div>
+ 
+ 
+ 
+            <div className = {styles.keejayContact}>
+                <div className={styles.logo}>
+                <Image src='/pfp.png' width={100} height={100}/>
+                </div>
+                <div>
+                    <h3 className = {styles.info}>Keejay Kim</h3>
+                    <div className={styles.links}>
+                        <a href='https://www.instagram.com/invites/contact/?i=1rasrnl6oit7b&utm_content=38yfz1h'>
+                            <Image src='/insta.png' width={40} height={40}/>
+                        </a>
+                        <a href='https://www.snapchat.com/add/keejaykim?share_id=L1zGgiZmtcQ&locale=en-US'>
+                            <Image src='/snap.png' width={40} height={40}/>
+                        </a>
+                        <a href='https://www.snapchat.com/add/keejaykim?share_id=L1zGgiZmtcQ&locale=en-US'>
+                            <Image src='/spotify-icon-0.png' width={40} height={40}/>
+                        </a>
+                    </div>
+                 
+                </div>
+ 
+            </div>
+        </div>
+ 
+    );
+}
+ 
+export default Contact;
+
