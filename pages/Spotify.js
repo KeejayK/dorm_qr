ef411e1fcc6f2d20919e4aa3628e6a87fa478c39
Keejay <KeejayK@gmail.com>
2022-08-26 19:28:08 -0700

    hi jae

---
+++ pages/Spotify.js
@@ -0,0 +1,9 @@
+const Spotify = () => {
+    return ( 
+        <div className=''>
+            <h1>test</h1>
+        </div>
+     );
+}
+ 
+export default Spotify;
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,15 @@
-import Head from 'next/head'
-import Image from 'next/image'
+
 import styles from '../styles/Home.module.css'
+
 import Contact from './Contact.js'
+import Spotify from './Spotify.js'
 
 
 export default function Home() {
   return (
     <div>
       <Contact/>
+      <Spotify/>
     </div>
   )
 }
