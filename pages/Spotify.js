dde78cbc5adc37a5e2b16b8e24adc4c199dee4c0
Keejay <KeejayK@gmail.com>
2022-08-26 20:56:43 -0700

    ngl forgot what i changed

--- pages/Spotify.js
+++ pages/Spotify.js
@@ -1,6 +1,9 @@
+import styles from '../styles/Spotify.module.css'
+
+
 const Spotify = () => {
     return ( 
-        <div className=''>
+        <div className={styles.spotify}>
             <h1>test</h1>
         </div>
      );
