3470da2bbc992150e7ca908e5d6fb084a661a7ca
Keejay <KeejayK@gmail.com>
2022-08-26 18:04:09 -0700

    fixed files

--- pages/Contact.js
+++ pages/Contact.js
@@ -7,19 +7,19 @@ const Contact = () => {
         <div className={styles.contact}>
             <div className={styles.jaeContact}>
             <div className={styles.logo}>
-                <Image src='/pfp.png' width={100} height={100}/>
+                <Image src='/profile.png' width={100} height={100}/>
                 </div>
                 <div>
                     <h3 className = {styles.info}>Jaechan Lee</h3>
                     <div className={styles.links}>
                         <a href='https://www.instagram.com/invites/contact/?i=1rasrnl6oit7b&utm_content=38yfz1h'>
-                            <Image src='/insta.png' width={40} height={40}/>
+                            <Image src='/instagram.png' width={40} height={40}/>
                         </a>
                         <a href='https://www.snapchat.com/add/keejaykim?share_id=L1zGgiZmtcQ&locale=en-US'>
-                            <Image src='/snap.png' width={40} height={40}/>
+                            <Image src='/snapchat.png' width={40} height={40}/>
                         </a>
                         <a href='https://www.snapchat.com/add/keejaykim?share_id=L1zGgiZmtcQ&locale=en-US'>
-                            <Image src='/spotify-icon-0.png' width={40} height={40}/>
+                            <Image src='/spotify.png' width={40} height={40}/>
                         </a>
                     </div>
                  
@@ -31,19 +31,19 @@ const Contact = () => {
  
             <div className = {styles.keejayContact}>
                 <div className={styles.logo}>
-                <Image src='/pfp.png' width={100} height={100}/>
+                <Image src='/profile.png' width={100} height={100}/>
                 </div>
                 <div>
                     <h3 className = {styles.info}>Keejay Kim</h3>
                     <div className={styles.links}>
                         <a href='https://www.instagram.com/invites/contact/?i=1rasrnl6oit7b&utm_content=38yfz1h'>
-                            <Image src='/insta.png' width={40} height={40}/>
+                            <Image src='/instagram.png' width={40} height={40}/>
                         </a>
                         <a href='https://www.snapchat.com/add/keejaykim?share_id=L1zGgiZmtcQ&locale=en-US'>
-                            <Image src='/snap.png' width={40} height={40}/>
+                            <Image src='/snapchat.png' width={40} height={40}/>
                         </a>
                         <a href='https://www.snapchat.com/add/keejaykim?share_id=L1zGgiZmtcQ&locale=en-US'>
-                            <Image src='/spotify-icon-0.png' width={40} height={40}/>
+                            <Image src='/spotify.png' width={40} height={40}/>
                         </a>
                     </div>
                  
