0b1b17bd9a777354ff8c592f2585051acd85d5c3
Keejay <KeejayK@gmail.com>
2022-08-27 02:15:09 -0700

    added current and last played template

--- pages/Spotify.js
+++ pages/Spotify.js
@@ -4,12 +4,19 @@ import styles from '../styles/Spotify.module.css'
 const Spotify = () => {
     return ( 
         <div className={styles.spotify}>
-            <div>
-
+            <div className={styles.jaeSpotify}>
+                <a className={styles.lable}>Currently Playing</a>
+                <h3 className={styles.current}>test 1</h3>
+                <a className={styles.lable}>Last Played</a>
+                <h3 className={styles.last}>test 1</h3>
+              
             </div>
-
-            <div>
-                
+            <div className={styles.keejaySpotify}>
+                <a className={styles.lable}>Currently Playing</a>
+                <h3 className={styles.current}>test 1</h3>
+                <a className={styles.lable}>Last Played</a>
+                <h3 className={styles.last}>test 1</h3>
+              
             </div>
         </div>
      );
