f073b84fe6a3a100aa1b3e9ea0f939a9ecbde5f2
Keejay <KeejayK@gmail.com>
2022-08-29 00:14:48 -0700

    centered message

--- pages/Message.js
+++ pages/Message.js
@@ -2,13 +2,21 @@ import styles from '../styles/Message.module.css'
 
 const Message = () => {
     return ( 
-        <div className={styles.messageContainer}>
-            <p>Leave us a message!</p>
-            <div className={styles.message}>
-                <form>
-                    <input type='submit' value='submit'/>
-                </form>
+        <div className={styles.main}> 
+            <div className={styles.messageContainer}>
+                <p>Leave us a message!</p>
+                <div className={styles.message}>
+                    <form>
+                        <input 
+                        id = 'name'
+                        name = 'message'
+                        type = 'textarea'
+                        />
+                    </form>
+                </div>
             </div>
+
+
         </div>
 
    );
